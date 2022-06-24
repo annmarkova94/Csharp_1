c8ee3f9c0e185514141a911b05b1128b6b699927
annmark94 <annmarkova94@gmail.com>
2022-06-25 02:04:10 +0300

    5 commit - conditions

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Write the user name: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "Masha")
+{
+    Console.WriteLine("Herey, this is Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
