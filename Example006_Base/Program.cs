d604b6d5351146654df77a366112f0ec81945762
annmark94 <annmarkova94@gmail.com>
2022-06-25 02:13:20 +0300

    6 commit - find max

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int maximum = a;
+
+if (b > maximum) maximum = b;
+if (c > maximum) maximum = c;
+if (d > maximum) maximum = d;
+if (e > maximum) maximum = e;
+
+Console.Write("Max = ");
+Console.WriteLine(maximum);
+
