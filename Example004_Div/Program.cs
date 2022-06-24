e70c39e3a70676cc1e93026d48d7f02d1ac1955b
annmark94 <annmarkova94@gmail.com>
2022-06-25 01:37:23 +0300

    4.2 commit - added random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = 10;
+double numberB = 4;
+double result = numberA / numberB;
+Console.WriteLine(result);
+
