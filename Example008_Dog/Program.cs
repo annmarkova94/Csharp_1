fc62b9a150bb469cdeec3daaab0c8b8c3025e645
annmark94 <annmarkova94@gmail.com>
2022-06-25 11:58:53 +0300

    8 added a dog task

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,25 @@
+﻿double speed_1_friend = 1;
+double speed_2_friend = 2;
+double speed_dog = 5;
+double time = 0;
+
+int friend = 2;
+double dist = 10000;
+int times_the_dog_run = 0;
+
+while (dist >= 10)
+{
+    if (friend == 1) {
+        time = dist / (speed_dog + speed_2_friend);
+        friend = 2;
+    }
+
+    if (friend == 2)  {
+        time = dist / (speed_dog + speed_1_friend);
+        friend = 1;
+    }
+    dist = dist - (speed_1_friend + speed_2_friend) * time;
+    times_the_dog_run++;
+}
+
+Console.WriteLine(times_the_dog_run);
