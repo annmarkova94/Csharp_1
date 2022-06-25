80235c6a54daf78907835a03b005d1bf23a17861
annmark94 <annmarkova94@gmail.com>
2022-06-25 11:59:18 +0300

    9 added a power task

---
+++ Example009_power_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Insert numberA: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Insert numberB: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberB == numberA * numberA)
+{
+    Console.WriteLine("Yes, numberB = numberA*numberA");
+}
+else
+{
+    Console.WriteLine("No, numberB != numberA*numberA");
+}
