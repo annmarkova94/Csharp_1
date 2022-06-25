9c85577bdec8e0e316dc99ce5a81062b04749178
annmark94 <annmarkova94@gmail.com>
2022-06-25 23:08:06 +0300

    Added a file with weekdays

---
+++ Example010_Weekdays/Program.cs
@@ -0,0 +1,12 @@
+﻿//Weekday, 1st way to do it
+Console.Write("Write a number from 0 to 6 for a weekday")
+int week = int.Parse(Console.ReadLine());
+if (week == 0) Console.WriteLine("Monday");
+if (week == 1) Console.WriteLine("Tuesday");
+if (week == 2) Console.WriteLine("Wednessday");
+if (week == 3) Console.WriteLine("Thursday");
+if (week == 4) Console.WriteLine("Friday");
+if (week == 5) Console.WriteLine("Sutarday");
+if (week == 6) Console.WriteLine("Sunday");
+
+//Weekday, 2nd way to do it
